a5001c3f18fa9b06ef9ec5126d5f052fdf4a732c
Sacha Sedriks <sachased@gmail.com>
2021-01-06 19:40:52 -0800

    Update / Position in Alphabet

--- Coding-Challenges/positionInAlphabet/positionInAlphabet.js
+++ Coding-Challenges/positionInAlphabet/positionInAlphabet.js
@@ -4,19 +4,20 @@ TASK:
 Given a letter, return its position in the alphabet.
 For example, given `'a'`, return `1`. Given `'Y'` return `25`.
 
+SOLUTION
+It's all about charCodes...
+
+Reference ASCII Table: https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
+Where a = 97 ~ z = 122, 
+
+1. make letter lowercase
+2. get letter charCode
+3. return charCode - 96 to get number between 1-26
+
 */
 
 function positionInAlphabet(letter) {
-
-    // Reference ASCII Table: https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
-    // Where a = 97 ~ z = 122, 
-
-    // 1. make letter lowercase
-    // 2. get letter charCode
-    // 3. return charCode - 96 to get number between 1-26
-
-    return `'${letter}': ${letter.toLowerCase().charCodeAt(0) - 96}`;
-
+    return [letter, letter.toLowerCase().charCodeAt(0) - 96].join(' ');
 }
 
 
